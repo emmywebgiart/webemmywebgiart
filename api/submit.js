046125e14d6e60ec0f8f57fc63fcad5771792ee7
Emmanuel Angeles - Emmy WEBGi ART <eangels1595@gmail.com>
2026-05-10 00:52:07 -0600

    fix submit pruebas

--- api/submit.js
+++ api/submit.js
@@ -1,43 +1,43 @@
-// import { GoogleSpreadsheet } from "google-spreadsheet";
-// import { JWT } from "google-auth-library";
+import { GoogleSpreadsheet } from "google-spreadsheet";
+import { JWT } from "google-auth-library";
 
-// export default async function handler(req, res) {
-//   if (req.method !== "POST") {
-//     return res.status(405).json({ success: false });
-//   }
-
-//   try {
-//     const { fullName, confirmation, guests, message } = req.body;
-
-//     const serviceAccountAuth = new JWT({
-//       email: process.env.GOOGLE_CLIENT_EMAIL,
-//       key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-//       scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-//     });
-
-//     const doc = new GoogleSpreadsheet(
-//       process.env.GOOGLE_SHEET_ID,
-//       serviceAccountAuth
-//     );
-
-//     await doc.loadInfo();
-//     const sheet = doc.sheetsByIndex[0];
-
-//     await sheet.addRow({
-//       Date: new Date().toISOString(),
-//       Name: fullName,
-//       Confirmation: confirmation,
-//       Guests: guests,
-//       Message: message,
-//     });
-
-//     return res.status(200).json({ success: true });
-
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ success: false });
-//   }
-// }
 export default async function handler(req, res) {
-  res.status(200).json({ ok: true });
+  if (req.method !== "POST") {
+    return res.status(405).json({ success: false });
+  }
+
+  try {
+    const { fullName, confirmation, guests, message } = req.body;
+
+    const serviceAccountAuth = new JWT({
+      email: process.env.GOOGLE_CLIENT_EMAIL,
+      key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
+
+    const doc = new GoogleSpreadsheet(
+      process.env.GOOGLE_SHEET_ID,
+      serviceAccountAuth
+    );
+
+    await doc.loadInfo();
+    const sheet = doc.sheetsByIndex[0];
+
+    await sheet.addRow({
+      Date: new Date().toISOString(),
+      Name: fullName,
+      Confirmation: confirmation,
+      Guests: guests,
+      Message: message,
+    });
+
+    return res.status(200).json({ success: true });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ success: false });
+  }
 }
+// export default async function handler(req, res) {
+//   res.status(200).json({ ok: true });
+// }
